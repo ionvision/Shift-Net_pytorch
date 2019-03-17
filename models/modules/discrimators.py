dd907d0139d49239295f519b6a574fa51cc918c3
Zhaoyi-Yan <yanzhaoyi@outlook.com>
2019-03-17 11:05:54 +0800

    another setting

--- models/modules/discrimators.py
+++ models/modules/discrimators.py
@@ -8,15 +8,15 @@ from .modules import *
 class NLayerDiscriminator(nn.Module):
     def __init__(self, input_nc, ndf=64, n_layers=3, norm_layer=nn.BatchNorm2d, use_sigmoid=False, use_spectral_norm=True):
         super(NLayerDiscriminator, self).__init__()
-        if type(norm_layer) == functools.partial:
-            use_bias = norm_layer.func == nn.InstanceNorm2d
-        else:
-            use_bias = norm_layer == nn.InstanceNorm2d
+        # if type(norm_layer) == functools.partial:
+        #     use_bias = norm_layer.func == nn.InstanceNorm2d
+        # else:
+        #     use_bias = norm_layer == nn.InstanceNorm2d
 
         kw = 4
         padw = 1
         sequence = [
-            spectral_norm(nn.Conv2d(input_nc, ndf, kernel_size=kw, stride=2, padding=padw), use_spectral_norm),
+            spectral_norm(nn.Conv2d(input_nc, ndf, kernel_size=kw, stride=2, padding=padw, bias=not use_spectral_norm), use_spectral_norm),
             nn.LeakyReLU(0.2, True)
         ]
 
@@ -27,7 +27,7 @@ class NLayerDiscriminator(nn.Module):
             nf_mult = min(2**n, 8)
             sequence += [
                 spectral_norm(nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult,
-                          kernel_size=kw, stride=2, padding=padw, bias=use_bias), use_spectral_norm),
+                          kernel_size=kw, stride=2, padding=padw, bias=not use_spectral_norm), use_spectral_norm),
                 norm_layer(ndf * nf_mult),
                 nn.LeakyReLU(0.2, True)
             ]
@@ -36,11 +36,11 @@ class NLayerDiscriminator(nn.Module):
         nf_mult = min(2**n_layers, 8)
         sequence += [
             spectral_norm(nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult,
-                      kernel_size=kw, stride=1, padding=padw, bias=use_bias), use_spectral_norm),
+                      kernel_size=kw, stride=1, padding=padw, bias=not use_spectral_norm), use_spectral_norm),
             norm_layer(ndf * nf_mult),
             nn.LeakyReLU(0.2, True)
         ]
-        sequence += [spectral_norm(nn.Conv2d(ndf * nf_mult, 1, kernel_size=kw, stride=1, padding=padw), use_spectral_norm)]
+        sequence += [spectral_norm(nn.Conv2d(ndf * nf_mult, 1, kernel_size=kw, stride=1, padding=padw, bias=not use_spectral_norm), use_spectral_norm)]
 
         if use_sigmoid:
             sequence += [nn.Sigmoid()]
